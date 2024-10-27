36443d92d2248aa4eb106d180016f28ed8193c94
ermmme <185312182+ermmme@users.noreply.github.com>
2024-10-27 11:03:51 -0700

    Finshed task 2.

--- script2.js
+++ script2.js
@@ -9,3 +9,12 @@
 // 14
 // FizzBuzz
 // ...
+
+for (let i = 1; i <= 100; i++) {
+  let output = "";
+
+  if (i % 3 === 0) output += "Fizz";
+  if (i % 5 === 0) output += "Buzz";
+
+  console.log(output || i);
+}
